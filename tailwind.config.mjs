642a09b0b91e3968464a3383c9d2481c0f47d750
tkndbj <tekin@ctscyprushomes.com>
2025-01-24 20:47:22 +0300

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 // tailwind.config.mjs
 
 export default {
+  darkMode: "class", // Enable class-based dark mode
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,12 +10,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'jade-green': '#00A86B',
+        "jade-green": "#00A86B",
         background: "var(--background)",
         foreground: "var(--foreground)",
         secondaryBackground: "var(--secondary-background)",
         accent: "var(--accent-color)",
-        // Add more if needed
+        // Add more custom colors if needed
       },
       fontFamily: {
         figtree: ['"Figtree"', "sans-serif"],
